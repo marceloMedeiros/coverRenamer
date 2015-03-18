b6946fc10a2a71f3ec3e68082d6b1c16c6ace961
marceloMedeiros <marcelomedeirosb@gmail.com>
2015-03-18 15:11:15 -0300

    Version 0.0001 pre-alpha

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dg = New System.Windows.Forms.DataGridView
+        Me.rompath = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.originalcoverpath = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.btnSearchRom = New System.Windows.Forms.Button
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog
+        Me.btnSearchCovers = New System.Windows.Forms.Button
+        Me.renamedcoverpath = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.btnSearchRenamed = New System.Windows.Forms.Button
+        Me.btnAnalize = New System.Windows.Forms.Button
+        Me.btnExecute = New System.Windows.Forms.Button
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.sttbProg = New System.Windows.Forms.ToolStripProgressBar
+        Me.sttbTxt = New System.Windows.Forms.ToolStripStatusLabel
+        Me.rom = New System.Windows.Forms.DataGridViewTextBoxColumn
+        Me.cover = New System.Windows.Forms.DataGridViewTextBoxColumn
+        Me.match = New System.Windows.Forms.DataGridViewCheckBoxColumn
+        CType(Me.dg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dg
+        '
+        Me.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dg.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.rom, Me.cover, Me.match})
+        Me.dg.Location = New System.Drawing.Point(12, 12)
+        Me.dg.Name = "dg"
+        Me.dg.Size = New System.Drawing.Size(905, 447)
+        Me.dg.TabIndex = 0
+        '
+        'rompath
+        '
+        Me.rompath.Location = New System.Drawing.Point(138, 468)
+        Me.rompath.Name = "rompath"
+        Me.rompath.Size = New System.Drawing.Size(520, 20)
+        Me.rompath.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 471)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Roms Path:"
+        '
+        'originalcoverpath
+        '
+        Me.originalcoverpath.Location = New System.Drawing.Point(138, 494)
+        Me.originalcoverpath.Name = "originalcoverpath"
+        Me.originalcoverpath.Size = New System.Drawing.Size(520, 20)
+        Me.originalcoverpath.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 497)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Original Covers Path:"
+        '
+        'btnSearchRom
+        '
+        Me.btnSearchRom.Location = New System.Drawing.Point(664, 466)
+        Me.btnSearchRom.Name = "btnSearchRom"
+        Me.btnSearchRom.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearchRom.TabIndex = 4
+        Me.btnSearchRom.Text = "Search"
+        Me.btnSearchRom.UseVisualStyleBackColor = True
+        '
+        'btnSearchCovers
+        '
+        Me.btnSearchCovers.Location = New System.Drawing.Point(664, 492)
+        Me.btnSearchCovers.Name = "btnSearchCovers"
+        Me.btnSearchCovers.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearchCovers.TabIndex = 5
+        Me.btnSearchCovers.Text = "Search"
+        Me.btnSearchCovers.UseVisualStyleBackColor = True
+        '
+        'renamedcoverpath
+        '
+        Me.renamedcoverpath.Location = New System.Drawing.Point(138, 520)
+        Me.renamedcoverpath.Name = "renamedcoverpath"
+        Me.renamedcoverpath.Size = New System.Drawing.Size(520, 20)
+        Me.renamedcoverpath.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 523)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Renamed Covers Path:"
+        '
+        'btnSearchRenamed
+        '
+        Me.btnSearchRenamed.Location = New System.Drawing.Point(664, 518)
+        Me.btnSearchRenamed.Name = "btnSearchRenamed"
+        Me.btnSearchRenamed.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearchRenamed.TabIndex = 6
+        Me.btnSearchRenamed.Text = "Search"
+        Me.btnSearchRenamed.UseVisualStyleBackColor = True
+        '
+        'btnAnalize
+        '
+        Me.btnAnalize.Location = New System.Drawing.Point(842, 490)
+        Me.btnAnalize.Name = "btnAnalize"
+        Me.btnAnalize.Size = New System.Drawing.Size(75, 23)
+        Me.btnAnalize.TabIndex = 7
+        Me.btnAnalize.Text = "Analize"
+        Me.btnAnalize.UseVisualStyleBackColor = True
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Location = New System.Drawing.Point(842, 517)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(75, 23)
+        Me.btnExecute.TabIndex = 8
+        Me.btnExecute.Text = "Execute"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.sttbProg, Me.sttbTxt})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 548)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(923, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'sttbProg
+        '
+        Me.sttbProg.AutoSize = False
+        Me.sttbProg.Name = "sttbProg"
+        Me.sttbProg.Size = New System.Drawing.Size(250, 16)
+        '
+        'sttbTxt
+        '
+        Me.sttbTxt.AutoSize = False
+        Me.sttbTxt.Name = "sttbTxt"
+        Me.sttbTxt.Size = New System.Drawing.Size(655, 17)
+        '
+        'rom
+        '
+        Me.rom.DataPropertyName = "rom"
+        Me.rom.HeaderText = "rom"
+        Me.rom.Name = "rom"
+        Me.rom.ReadOnly = True
+        Me.rom.Width = 395
+        '
+        'cover
+        '
+        Me.cover.DataPropertyName = "cover"
+        Me.cover.HeaderText = "cover"
+        Me.cover.Name = "cover"
+        Me.cover.ReadOnly = True
+        Me.cover.Width = 395
+        '
+        'match
+        '
+        Me.match.DataPropertyName = "match"
+        Me.match.FalseValue = "N"
+        Me.match.HeaderText = "match"
+        Me.match.IndeterminateValue = "N"
+        Me.match.Name = "match"
+        Me.match.ReadOnly = True
+        Me.match.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        Me.match.TrueValue = "S"
+        Me.match.Width = 40
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(923, 570)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnExecute)
+        Me.Controls.Add(Me.btnAnalize)
+        Me.Controls.Add(Me.btnSearchRenamed)
+        Me.Controls.Add(Me.btnSearchCovers)
+        Me.Controls.Add(Me.btnSearchRom)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.renamedcoverpath)
+        Me.Controls.Add(Me.originalcoverpath)
+        Me.Controls.Add(Me.rompath)
+        Me.Controls.Add(Me.dg)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Cover Renamer for WiiFlow By Marcelo Medeiros"
+        CType(Me.dg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dg As System.Windows.Forms.DataGridView
+    Friend WithEvents rompath As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents originalcoverpath As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSearchRom As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents btnSearchCovers As System.Windows.Forms.Button
+    Friend WithEvents renamedcoverpath As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnSearchRenamed As System.Windows.Forms.Button
+    Friend WithEvents btnAnalize As System.Windows.Forms.Button
+    Friend WithEvents btnExecute As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents sttbProg As System.Windows.Forms.ToolStripProgressBar
+    Friend WithEvents sttbTxt As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents rom As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cover As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents match As System.Windows.Forms.DataGridViewCheckBoxColumn
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        rompath.Text = "H:\snes9xgx\roms"
+        originalcoverpath.Text = "D:\#temp\Wii\covers\SNES Vert Shrunken Spine (753)\SNES Vert Shrunken Spine (753)\all"
+        renamedcoverpath.Text = "D:\#temp\Wii\covers\wiiflow renamed\vert"
+    End Sub
+
+
+
+    Private Sub btnSearchRom_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearchRom.Click
+        FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer
+        FolderBrowserDialog1.SelectedPath = rompath.Text
+        FolderBrowserDialog1.ShowDialog()
+        rompath.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+    Private Sub btnSearchCovers_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearchCovers.Click
+        FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer
+        FolderBrowserDialog1.SelectedPath = originalcoverpath.Text
+        FolderBrowserDialog1.ShowDialog()
+        originalcoverpath.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+    Private Sub btnSearchRenamed_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearchRenamed.Click
+        FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer
+        FolderBrowserDialog1.SelectedPath = renamedcoverpath.Text
+        FolderBrowserDialog1.ShowDialog()
+        renamedcoverpath.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+    Private Sub btnAnalize_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAnalize.Click
+        analize()
+    End Sub
+
+
+
+    Private Sub analize()
+        Dim dt As New DataTable
+
+        dt.Columns.Add("rom", GetType(String))
+        dt.Columns.Add("romParsed", GetType(String))
+        dt.Columns.Add("cover", GetType(String))
+        dt.Columns.Add("match", GetType(String))
+
+        dt.Columns.Add("game", GetType(String))
+        dt.Columns.Add("word0", GetType(String))
+        dt.Columns.Add("word1", GetType(String))
+        dt.Columns.Add("word2", GetType(String))
+        dt.Columns.Add("word3", GetType(String))
+        dt.Columns.Add("word4", GetType(String))
+        dt.Columns.Add("word5", GetType(String))
+        dt.Columns.Add("word6", GetType(String))
+        dt.Columns.Add("word7", GetType(String))
+        dt.Columns.Add("word8", GetType(String))
+        dt.Columns.Add("word9", GetType(String))
+        dt.Columns.Add("attribute0", GetType(String))
+        dt.Columns.Add("attribute1", GetType(String))
+        dt.Columns.Add("attribute2", GetType(String))
+        dt.Columns.Add("attribute3", GetType(String))
+        dt.Columns.Add("attribute4", GetType(String))
+        dt.Columns.Add("attribute5", GetType(String))
+        dt.Columns.Add("attribute6", GetType(String))
+        dt.Columns.Add("attribute7", GetType(String))
+        dt.Columns.Add("attribute8", GetType(String))
+        dt.Columns.Add("attribute9", GetType(String))
+        dt.Columns.Add("index", GetType(Integer))
+
+        Dim dtCovers As New DataTable
+        dtCovers.Columns.Add("cover", GetType(String))
+        dtCovers.Columns.Add("coverParsed", GetType(String))
+        dtCovers.Columns.Add("game", GetType(String))
+        dtCovers.Columns.Add("word0", GetType(String))
+        dtCovers.Columns.Add("word1", GetType(String))
+        dtCovers.Columns.Add("word2", GetType(String))
+        dtCovers.Columns.Add("word3", GetType(String))
+        dtCovers.Columns.Add("word4", GetType(String))
+        dtCovers.Columns.Add("word5", GetType(String))
+        dtCovers.Columns.Add("word6", GetType(String))
+        dtCovers.Columns.Add("word7", GetType(String))
+        dtCovers.Columns.Add("word8", GetType(String))
+        dtCovers.Columns.Add("word9", GetType(String))
+        dtCovers.Columns.Add("attribute0", GetType(String))
+        dtCovers.Columns.Add("attribute1", GetType(String))
+        dtCovers.Columns.Add("attribute2", GetType(String))
+        dtCovers.Columns.Add("attribute3", GetType(String))
+        dtCovers.Columns.Add("attribute4", GetType(String))
+        dtCovers.Columns.Add("attribute5", GetType(String))
+        dtCovers.Columns.Add("attribute6", GetType(String))
+        dtCovers.Columns.Add("attribute7", GetType(String))
+        dtCovers.Columns.Add("attribute8", GetType(String))
+        dtCovers.Columns.Add("attribute9", GetType(String))
+        dtCovers.Columns.Add("index", GetType(Integer))
+
+        'scan rom path to get all romnames
+        For Each file As String In My.Computer.FileSystem.GetFiles(rompath.Text)
+            Dim newLine As DataRow = dt.NewRow
+            With newLine
+                .Item("rom") = file.Replace(rompath.Text, "").Replace("\", "")
+                .Item("game") = Trim(.Item("rom"))
+                .Item("game") = Mid(.Item("game"), 1, .Item("game").ToString.LastIndexOf(".")) 'remove the extension
+
+                Dim strToSplit As String = Trim(.Item("game"))
+                Dim strSplit As String() = strToSplit.Split(" ")
+
+                Dim indexx As Integer = 0
+                For i As Integer = 0 To strSplit.Length - 1
+                    If i > 9 Then Exit For
+                    If Not (strSplit(i).Contains("(") Or strSplit(i).Contains(")") Or strSplit(i).Contains("[") Or strSplit(i).Contains("]")) Then
+                        .Item("word" & indexx) = parse(strSplit(i))
+                        indexx += 1
+                    End If
+                Next
+
+                indexx = 0
+                For i As Integer = 0 To strSplit.Length - 1
+                    If indexx > 9 Then Exit For
+                    If (strSplit(i).Contains("(") Or strSplit(i).Contains("[")) Then
+                        .Item("attribute" & indexx) = strSplit(i)
+                        indexx += 1
+                    ElseIf (strSplit(i).Contains(")") Or strSplit(i).Contains("]")) Then
+                        .Item("attribute" & indexx - 1) &= " " & strSplit(i)
+                    End If
+                Next
+            End With
+            dt.Rows.Add(newLine)
+        Next
+
+
+        'then, scan the cover path, and generate a list with all covers
+        For Each file As String In My.Computer.FileSystem.GetFiles(originalcoverpath.Text)
+            Dim newLine As DataRow = dtCovers.NewRow
+            With newLine
+                .Item("cover") = file.Replace(originalcoverpath.Text, "").Replace("\", "")
+                .Item("game") = Trim(.Item("cover"))
+                .Item("game") = Mid(.Item("game"), 1, .Item("game").ToString.LastIndexOf(".")) 'remove the extension
+
+                Dim strToSplit As String = Trim(.Item("game"))
+                Dim strSplit As String() = strToSplit.Split(" ")
+
+                Dim indexx As Integer = 0
+                For i As Integer = 0 To strSplit.Length - 1
+                    If i > 9 Then Exit For
+                    If Not (strSplit(i).Contains("(") Or strSplit(i).Contains(")") Or strSplit(i).Contains("[") Or strSplit(i).Contains("]")) Then
+                        .Item("word" & indexx) = parse(strSplit(i))
+                        indexx += 1
+                    End If
+                Next
+
+                indexx = 0
+                For i As Integer = 0 To strSplit.Length - 1
+                    If indexx > 9 Then Exit For
+                    If (strSplit(i).Contains("(") Or strSplit(i).Contains("[")) Then
+                        .Item("attribute" & indexx) = strSplit(i)
+                        indexx += 1
+                    ElseIf (strSplit(i).Contains(")") Or strSplit(i).Contains("]")) Then
+                        .Item("attribute" & indexx - 1) &= " " & strSplit(i)
+                    End If
+                Next
+            End With
+            dtCovers.Rows.Add(newLine)
+        Next
+
+        'now, we try to match the roms with its covers
+
+        For i As Integer = 0 To dt.Rows.Count - 1
+            'For Each rom As DataRow In dt.Rows
+            Dim rom As DataRow = dt.Rows(i)
+            Dim coverFile As String
+            coverFile = locateCover(i, _
+                                    dt, _
+                                    dtCovers)
+            rom.Item("cover") = coverFile
+            If Not coverFile Is Nothing AndAlso coverFile.Length > 0 Then
+                rom.Item("match") = "S"
+            End If
+        Next
+
+
+        dg.AutoGenerateColumns = False
+        dg.DataSource = dt
+
+    End Sub
+    Private Function locateCover(ByVal indexDT As Integer, _
+                                 ByVal dtRoms As DataTable, _
+                                 ByVal tableCovers As DataTable)
+        Dim coverFile As String = ""
+        Dim count As Integer = 0
+        Dim result As Integer = 0
+
+        Dim accuracy As Integer = 75
+
+        'try to find an exact match
+        count = tableCovers.Compute(" count(game)", " game ='" & parse(dtRoms.Rows(indexDT).Item("game")) & "'")
+
+        Select Case count
+            Case 0 'no exact match, so we need to start comparing the file names, word by word 
+
+                Dim romWords As New ArrayList
+                Dim dtCoverFiltered As DataTable = tableCovers
+
+                'First, we need to get all the words of the rom gamename
+                For i As Integer = 0 To 9
+                    If Not IsDBNull(dtRoms.Rows(indexDT).Item("word" & i)) Then
+                        romWords.Add(dtRoms.Rows(indexDT).Item("word" & i))
+                    End If
+                Next
+
+                'Then, we are gonna look for the cover for this game, word by word
+                For i As Integer = 0 To romWords.Count - 1
+                    dtCoverFiltered.DefaultView.RowFilter = " word" & i & " = '" & parse(romWords(i)) & "'"
+                    Select Case dtCoverFiltered.DefaultView.ToTable.Rows.Count
+                        Case 0
+                            'Ooops! no cover found with the last filter
+                            Exit For
+                        Case 1
+                            'We found our cover! (hopefully)
+
+                            'To make sure the cover selected is correct, lets look at the next word in the cover selected
+                            ' if thats the correct cover, it should be empty
+
+                            If Not romWords.Count = 10 Then
+                                If Not IsDBNull(dtCoverFiltered.DefaultView.ToTable.Rows(0).Item("word" & romWords.Count)) Then
+                                    Continue For
+                                End If
+                            End If
+
+                            coverFile = dtCoverFiltered.DefaultView.ToTable.Rows(0).Item("cover")
+                            Exit For
+                        Case Else
+                            'More than one result = we need to add more filters
+                            dtCoverFiltered = dtCoverFiltered.DefaultView.ToTable
+                    End Select
+                Next
+
+
+            Case 1 'we've found a exact match. Hooray!
+                coverFile = tableCovers.Compute(" max(cover)", " game ='" & parse(dtRoms.Rows(indexDT).Item("game")) & "'")
+
+            Case Else
+
+        End Select
+
+
+        'if we cannot find an exact match, lets try a less acurate search
+
+
+        Return coverFile
+    End Function
+    Private Function parse(ByVal str As String) As String
+        Return str.Replace("'", "")
+    End Function
+
+
+    Private Sub btnExecute_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExecute.Click
+
+        For Each dr As DataRow In CType(dg.DataSource, DataTable).Select(" match='S'", "")
+
+            Dim sourceFile, destinationFile, coverExtension As String
+
+            sourceFile = originalcoverpath.Text
+            If Not Mid(sourceFile, sourceFile.Length, 1) = "/" Then
+                sourceFile &= "\"
+            End If
+            sourceFile &= dr.Item("cover")
+
+            coverExtension = Mid(sourceFile, sourceFile.ToString.LastIndexOf(".") + 1) 'remove the extension
+
+            destinationFile = renamedcoverpath.Text
+            If Not Mid(destinationFile, destinationFile.Length, 1) = "/" Then
+                destinationFile &= "\"
+            End If
+            destinationFile &= dr.Item("rom") & coverExtension
+
+
+            My.Computer.FileSystem.CopyFile(sourceFile, destinationFile)
+
+
+        Next
+
+
+    End Sub
+End Class
